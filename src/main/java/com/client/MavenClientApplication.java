d28dc3803ecb6ef8ac60a0861af308e6f7d017c1
Rahulstar177 <rathodsachiit@gmaiil.com>
2025-02-07 17:26:23 +0530

    this is test

---
+++ src/main/java/com/client/MavenClientApplication.java
@@ -0,0 +1,20 @@
+package com.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.buildmaven.add.Sum;
+
+@SpringBootApplication
+public class MavenClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenClientApplication.class, args);
+
+		Sum sum = new Sum();
+
+		int addition = sum.addition(10, 15);
+		System.out.println("Addition "+addition);
+	}
+
+}
