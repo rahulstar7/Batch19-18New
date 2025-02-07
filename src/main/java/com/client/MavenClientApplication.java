88a2e63f492841006d5affaf23c4e5186324ea2b
Rahulstar177 <rathodsachiit@gmaiil.com>
2025-02-07 17:32:12 +0530

    this is developer commit

--- src/main/java/com/client/MavenClientApplication.java
+++ src/main/java/com/client/MavenClientApplication.java
@@ -14,10 +14,16 @@ public class MavenClientApplication {
 		Sum sum = new Sum();
 
 		int addition = sum.addition(10, 15);
-		System.out.println("Addition "+addition);
+		System.out.println("Addition " + addition);
 	}
+
 public String qaMethod()
 {
 return "BikkadIT";
-}
+
+	public String devMethod() {
+
+		return "this is developer";
+
+	}
 }
